669517f324a3a4f14c58350ee2abe3070c47e508
liuyuxue <liuyx1982@163.com>
2018-04-10 18:05:47 +0800

    test1 add a dir and a file named KV.cs

---
+++ testpro1/KV.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winsion.CT.Data
+{
+    public struct KV
+    {
+        public string K { get; set; }
+        public string V { get; set; }
+    }
+}
